31ccd797b7a4661cb3be1eb801adef64843ceed5
Fristi <mark@vectos.net>
2023-07-06 17:53:36 +0200

    Backend build?

--- backend/build.sbt
+++ backend/build.sbt
@@ -1,6 +1,9 @@
 scalaVersion := "2.13.10"
-name := "scala-example"
-organization := "earthly.dev"
+name := "backend"
+organization := "co.mycelium"
 version := "1.0"
 
-libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
+libraryDependencies ++= Seq(
+    "org.http4s" %% "http4s-ember-server" % "0.23.18",
+    "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % "1.6.0"
+)
--- backend/src/main/scala/co/mycelium/Main.scala
+++ backend/src/main/scala/co/mycelium/Main.scala
@@ -1,3 +1,28 @@
-object Main extends App {
-  println("Hello, World!")
+package co.mycelium
+
+import cats.effect._
+import com.comcast.ip4s._
+import org.http4s.HttpApp
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Server
+import org.http4s.server.staticcontent._
+import org.typelevel.log4cats.LoggerFactory
+import org.typelevel.log4cats.slf4j.Slf4jFactory
+
+object Main extends IOApp {
+
+  implicit val loggerFactory: LoggerFactory[IO] = Slf4jFactory.create[IO]
+  override def run(args: List[String]): IO[ExitCode] =
+    app.use(_ => IO.never).as(ExitCode.Success)
+
+  val httpApp: HttpApp[IO] =
+    fileService[IO](FileService.Config(".")).orNotFound
+
+  val app: Resource[IO, Server] =
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(httpApp)
+      .build
 }
